7a7379e870dcdaf0e0c93dc289e2b9b4aa412c80
Willianrocha1995 <willian1995rocha@gmail.com>
2025-07-31 16:34:39 -0300

    Ajuste Readme

--- cypress/e2e/bugbank.cy.js
+++ cypress/e2e/bugbank.cy.js
@@ -1,60 +1,51 @@
-describe('template spec', () => {
+describe('template bugbank', () => {
+
+  let dados;
 
   beforeEach(() => {
+    cy.fixture('example').then((data) => {
+      dados = data;
+    });
 
-    cy.visit('/')
+    cy.visit('/');
     cy.get('#__next > div > div > h1')
       .should('be.visible')
-      .should('have.text', 'O banco com bugs e falhas do seu jeito')
-  })
+      .should('have.text', 'O banco com bugs e falhas do seu jeito');
+  });
 
   it('Cadastro de usuário com sucesso', () => {
-    
-    const email = 'teste@teste.com'
-    const name = 'Carlos da Silva'
-    const password = '123@Xyz'
+    const { email, nome, senha } = dados.usuarioValido;
 
-    cy.userRegistration(email, name, password, password, false)
-    cy.get('svg[color="green"]').should('be.visible')
+    cy.userRegistration(email, nome, senha, senha, false);
+    cy.get('svg[color="green"]').should('be.visible');
     cy.get('#modalText').contains(/^A conta \d{1,3}-\d foi criada com sucesso$/).should('be.visible');
-    cy.contains('Fechar').click()
+    cy.contains('Fechar').click();
 
-    cy.login(email, password)
-
-    cy.url().should('match', /home$/)
-    cy.get('#textName').should('have.text', 'Olá ' + name + ',')
-
-  })
+    cy.login(email, senha);
+    cy.url().should('match', /home$/);
+    cy.get('#textName').should('have.text', 'Olá ' + nome + ',');
+  });
 
   it('Cadastro de usuário com dados inválidos - nome não informado', () => {
+    const { email, senha } = dados.usuarioSemNome;
 
-    cy.userRegistration('teste@teste.com', '', '123@Xyz', '123@Xyz', true)
-    cy.get('svg[color="red"]').should('be.visible')
-    cy.get('#modalText').should('have.text', 'Nome não pode ser vazio.\n')
-    cy.contains('Fechar').click()
-
-  })
+    cy.userRegistration(email, '', senha, senha, true);
+    cy.get('svg[color="red"]').should('be.visible');
+    cy.get('#modalText').should('have.text', 'Nome não pode ser vazio.\n');
+    cy.contains('Fechar').click();
+  });
 
   it('Transferência para uma conta inexistente', () => {
+    const { email, nome, senha } = dados.usuarioValido;
+    const contaInvalida = { numero: '000', digito: '1' };
 
-    const email = 'teste@teste.com'
-    const name = 'Carlos da Silva'
-    const password = '123@Xyz'
-    const accountNumber = '000'
-    const digit = '1'
-    const transferValue = '100'
-    const description = 'teste'
+    cy.userRegistration(email, nome, senha, senha, false);
+    cy.contains('Fechar').click();
 
-    cy.userRegistration(email, name, password, password, false)
-    cy.contains('Fechar').click()
+    cy.login(email, senha);
 
-    cy.login(email, password)
-
-    cy.transferValue(accountNumber, digit, transferValue, description)
-
-    cy.get('#modalText').should('have.text', 'Conta inválida ou inexistente')
-    cy.contains('Fechar').click()
-
-  })
-
-})
+    cy.transferValue(contaInvalida.numero, contaInvalida.digito, '100', 'teste');
+    cy.get('#modalText').should('have.text', 'Conta inválida ou inexistente');
+    cy.contains('Fechar').click();
+  });
+});
