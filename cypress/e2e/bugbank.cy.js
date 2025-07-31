ac2a829621aa6d5cc651584be91461f1ae07fdef
Willianrocha1995 <willian1995rocha@gmail.com>
2025-07-31 16:02:31 -0300

    Ajuste Readme

--- cypress/e2e/bugbank.cy.js
+++ cypress/e2e/bugbank.cy.js
@@ -1,64 +1,60 @@
-/// <reference types="cypress" />
+describe('template spec', () => {
 
-describe('login',() =>{
+  beforeEach(() => {
 
-beforeEach(() => {
-
-cy.visit('/')
-cy.get('#__next > div > div > h1')
-.should('be.visible')
-.should('have.text', 'O banco com bugs e falhas do seu jeito')
+    cy.visit('/')
+    cy.get('#__next > div > div > h1')
+      .should('be.visible')
+      .should('have.text', 'O banco com bugs e falhas do seu jeito')
   })
 
-//Cenário 1 - Fluxos Básicos
-it('Cadastro de Usuário com Sucesso', () => {
-    cy.get('.ihdmxA').click()
-    cy.get(':nth-child(2) > .input__default').type('willian@gmail.com',{force:true})
-    cy.get(':nth-child(3) > .input__default').type('willian rocha',{force:true})
-    cy.get(':nth-child(4) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type('senha123',{force:true})
-    cy.get(':nth-child(5) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type('senha123',{force:true})
-    cy.get('label#toggleAddBalance').click({ force: true })
-    cy.contains('Cadastrar').click({ force: true })
+  it('Cadastro de usuário com sucesso', () => {
+    
+    const email = 'teste@teste.com'
+    const name = 'Carlos da Silva'
+    const password = '123@Xyz'
+
+    cy.userRegistration(email, name, password, password, false)
+    cy.get('svg[color="green"]').should('be.visible')
     cy.get('#modalText').contains(/^A conta \d{1,3}-\d foi criada com sucesso$/).should('be.visible');
     cy.contains('Fechar').click()
-})
 
+    cy.login(email, password)
 
-//Cenário 1 - Fluxos Adicionais
-it('Cadastro de usuário com dados inválidos', () => {
-    cy.get('.ihdmxA').click()
-    cy.get(':nth-child(2) > .input__default').type('willian',{force:true})
-    cy.get(':nth-child(3) > .input__default').type('willian rocha',{force:true})
-    cy.get(':nth-child(4) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type('senha123',{force:true})
-    cy.get(':nth-child(5) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type('senha123',{force:true})
-    cy.get('label#toggleAddBalance').click({ force: true })
-    cy.contains('Cadastrar').click({ force: true })
-    cy.get('.input__warging').should('contain.text', 'Formato inválido')
-})
+    cy.url().should('match', /home$/)
+    cy.get('#textName').should('have.text', 'Olá ' + name + ',')
 
-//Cenário 2 - Fluxos Adicionais
-it('Transferência para uma conta inexistente', () => {
-    cy.get('.ihdmxA').click()
-    cy.get(':nth-child(2) > .input__default').type('willian@gmail.com',{force:true})
-    cy.get(':nth-child(3) > .input__default').type('willian rocha',{force:true})
-    cy.get(':nth-child(4) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type('senha123',{force:true})
-    cy.get(':nth-child(5) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type('senha123',{force:true})
-    cy.get('label#toggleAddBalance').click({ force: true })
-    cy.contains('Cadastrar').click({ force: true })
-    cy.get('#modalText').contains(/^A conta \d{1,3}-\d foi criada com sucesso$/).should('be.visible');
+  })
+
+  it('Cadastro de usuário com dados inválidos - nome não informado', () => {
+
+    cy.userRegistration('teste@teste.com', '', '123@Xyz', '123@Xyz', true)
+    cy.get('svg[color="red"]').should('be.visible')
+    cy.get('#modalText').should('have.text', 'Nome não pode ser vazio.\n')
     cy.contains('Fechar').click()
-    cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > :nth-child(1) > .input__default').type('willian@gmail.com')
-    cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > .login__password > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type('senha123')
-    cy.get('.otUnI').click()
-    cy.get('#textName').should('contain.text', 'willian rocha')
-    cy.get('#textBalance > span').should('contain.text', '1.000,00')
-    cy.get('#btn-TRANSFERÊNCIA').click()
-    cy.get(':nth-child(1) > .input__default').type('12345')
-    cy.get('.account__data > :nth-child(2) > .input__default').type('7')
-    cy.get('.styles__ContainerFormTransfer-sc-1oow0wh-0 > :nth-child(2) > .input__default').type('200')
-    cy.get(':nth-child(3) > .input__default').type('Descrição da Transação')
-    cy.get('.style__ContainerButton-sc-1wsixal-0').click()
+
+  })
+
+  it('Transferência para uma conta inexistente', () => {
+
+    const email = 'teste@teste.com'
+    const name = 'Carlos da Silva'
+    const password = '123@Xyz'
+    const accountNumber = '000'
+    const digit = '1'
+    const transferValue = '100'
+    const description = 'teste'
+
+    cy.userRegistration(email, name, password, password, false)
+    cy.contains('Fechar').click()
+
+    cy.login(email, password)
+
+    cy.transferValue(accountNumber, digit, transferValue, description)
+
     cy.get('#modalText').should('have.text', 'Conta inválida ou inexistente')
     cy.contains('Fechar').click()
-})
+
+  })
+
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,49 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('userRegistration', (email, name, password, passwordConfirmation, emptyName) => {
+
+    cy.contains('Registrar').click()
+    cy.get('div.card__register input[type="email"]')
+        .click({ force: true })
+        .type(email)
+
+    if (emptyName == false) {
+        cy.get('div.card__register input[type="name"]')
+            .click({ force: true })
+            .type(name)
+    }
+
+    cy.get('div.card__register input[name="password"]')
+        .click({ force: true })
+        .type(password, { log: false })
+    cy.get('div.card__register input[name="passwordConfirmation"]')
+        .click({ force: true })
+        .type(passwordConfirmation, { log: false })
+
+    cy.get('#toggleAddBalance').click({ force: true })
+
+    cy.contains('Cadastrar').click({ force: true })
+
+
+})
+
+Cypress.Commands.add('login', (email, password) => {
+
+    cy.get('div.card__login input[type="email"]').type(email)
+    cy.get('div.card__login input[name="password"]').type(password, { log: false })
+    cy.contains('Acessar').click()
+
+})
+
+Cypress.Commands.add('transferValue', (accountNumber, digit, transferValue, description) => {
+
+    cy.get('#btn-TRANSFERÊNCIA').click()
+    cy.get('input[type="accountNumber"]').type(accountNumber)
+    cy.get('input[type="digit"]').type(digit)
+    cy.get('input[type="transferValue"]').type(transferValue)
+    cy.get('input[type="description"]').type(description)
+    cy.contains('Transferir agora').click()
+
+})
