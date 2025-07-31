e246718e9385143fc131294805cf0e88f4e61865
Willianrocha1995 <willian1995rocha@gmail.com>
2025-07-31 12:58:53 -0300

    Adicionando tempo de espera

--- cypress.config.js
+++ cypress.config.js
@@ -17,12 +17,12 @@ module.exports = defineConfig({
   },
   chromeWebSecurity: false,
   video: true,
+  pageLoadTimeout: 120000, // espera até 2 minutos para o carregamento da página
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on)
+      require('cypress-mochawesome-reporter/plugin')(on);
       config.baseUrl = config.env.baseUrl;
       return config;
-      // implement node event listeners here
     },
   },
 });
